58d4232739ef61978efe1a1ecd703c23ae4a5ff3
CaioCDJ <caiodjesus1@hotmail.com>
2022-07-01 16:45:20 -0300

    adding React

--- ex/index.js
+++ ex/index.js
@@ -1,20 +1,31 @@
 //const logger = require('./logger');
 
 
-
 // logger.info('Usando o padrao commonJS');
 
-// sem babel 2015
+// ex:2 sem babel 2015
 const Pessoa = require('../ex.2/index.js');
 
 const pessoa = new Pessoa('Guilherme');
 console.log(pessoa.toString())
 
 
-// com bable
+//ex:3 com babel
 import Pessoa2 from '../ex.3/index.js'
 
 const pessoa2 = new Pessoa2('Roberta');
 console.log(pessoa2.toString())
 
+// exercicio 4
+const ex = require('../ex.4/index.js')
+
+// react
+
+import 'react'
+
+export default props => (
+  <h1>Ola</h1>
+)
+
+console.log('Funcionou!')
 
